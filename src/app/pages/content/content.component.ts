3a6e9b73f7192a4d5eb214497c505d2446b25ed2
Justine Romero <thepiesaresquared@gmail.com>
2021-08-03 02:46:28 +0800

    feat: disable some links

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,12 +10,22 @@ import { HttpClient } from '@angular/common/http';
 export class ContentComponent implements OnInit {
   url: string = '';
   navigationSubscription;
+  isActivation = false;
+  activationLink: string = '';
 
-  constructor(private router: Router, private http: HttpClient) {
-    this.url = this.router.url.split('#')[0];
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
+    this.isActivation = this.route.snapshot.queryParams['ac'];
+    this.activationLink = this.route.snapshot.queryParams['ac'];
+
+    this.url = this.router.url.split('#')[0].split('?')[0];
+
     this.navigationSubscription = this.router.events.subscribe((e: any) => {
       if (e instanceof NavigationEnd) {
-        this.url = this.router.url.split('#')[0];
+        this.url = this.router.url.split('#')[0].split('?')[0];
       }
     });
   }
